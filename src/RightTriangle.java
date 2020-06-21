ec7c27a88632b5d4635507d44844bd3257e73715
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-21 08:00:39 -0400

    comments deleted

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -9,17 +9,5 @@ public class RightTriangle {
         int hypotenuse3 = triangle.side3 * triangle.side3;
         return (hypotenuse == result) || (hypotenuse2 == result2) || (hypotenuse3 == result3);
     }
-    /*
-    boolean isRightTriangle1(Triangle triangle) {
-        int result2 = triangle.side3 * triangle.side3 + triangle.side1 * triangle.side1;
-        int hypotenuse2 = triangle.side2 * triangle.side2;
-        return hypotenuse2 == result2;
-    }
-    boolean isRightTriangle2(Triangle triangle) {
-        int result3 = triangle.side1 * triangle.side1 + triangle.side2 * triangle.side2;
-        int hypotenuse3 = triangle.side3 * triangle.side3;
-        return hypotenuse3 == result3;
-    }
-*/
 
     }
--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        Triangle triangle1 = new Triangle(4,5,2);
+        Triangle triangle1 = new Triangle(4,5,3);
         RightTriangle triangle = new RightTriangle();
         triangle.isRightTriangle(triangle1);
-        /*
-        triangle.isRightTriangle1(triangle1);
-        triangle.isRightTriangle2(triangle1);
 
-         */
         System.out.println(triangle.isRightTriangle(triangle1));
-        /*
-        System.out.println(triangle.isRightTriangle1(triangle1));
-        System.out.println(triangle.isRightTriangle2(triangle1));
 
-         */
     }
 }
